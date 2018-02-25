7a80ff62852d838744ee767c38cbe19b0ce26df2
batoolabuali <batool_abuali@yahoo.com>
2018-02-26 00:19:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/musicplayer3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.musicplayer3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.android.musicplayer3.R.layout.activity_main);
+    }
+}
